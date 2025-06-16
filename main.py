40c247222dcaa011af1cc867e9d864c514a81727
LucaLau26 <mingfunglau@kellettschool.com>
2025-06-16 01:44:41 +0000

    Jun 16, 2025, 1:44 AM

---
+++ main.py
@@ -0,0 +1,20 @@
+print("WELCOME TO PALE LUNA")
+print()
+print("Before you play, commands go like this: 'go_north' and 'pick_up_(item)' ")
+print()
+start = input("Ready to play? (play_game/dont_play_game) ")
+
+if start == "dont_play_game":
+    pass
+
+elif start == "play_game":
+    gamestart = True
+
+while gamestart:
+    print("You are in a dark room. Moonlight shines through the window.")
+    print()
+    print("There is a shovel to the south, and some gold to the west.")
+    print()
+    print("There is also a door to the east.")
+    print()
+    input("Command? ")
