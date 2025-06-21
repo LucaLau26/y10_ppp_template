104b5dbbfc449664fc9b4d71231ff7917ed7a8f3
LucaLau26 <mingfunglau@kellettschool.com>
2025-06-21 08:42:30 +0000

    Jun 21, 2025, 8:42 AM

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ elif start == "playgame":
                     print()
                     print("The ground is soft.")
                     print()
-                    print("\033[31mPALE LUNA SMILES WIDE.\033[31m")
+                    print("\033[31mPALE LUNA SMILES WIDE.")
                     print()
                     print("Here.")
                     print()
